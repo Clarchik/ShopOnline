e305d8bd3ca238c464f420931014b87fc21ecc71
Vitalii Burakovskyy <vitalii.burakovskyy@stazysta.comarch.pl>
2019-10-04 09:03:13 +0200

    Code writing stucture

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'OnlineShop';
-  testing = 32;
+    title = 'OnlineShop';
+    testing = 32;
+    constructor() {
+        const er = new Object();
+    }
 }
