48061b20a74e0e35ebd39929b9d760c5cf6511f3
RudolfKlikh <rudolfklikh@gmail.com>
2019-10-22 00:33:33 +0200

    WORK-3 (Implementation for Slider Layout)

---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Slides } from '../../shared/models/slider-json/slides';
+import { Slide } from '../../shared/interfaces/slide-interface';
+@Component({
+    selector: 'app-carousel',
+    templateUrl: './carousel.component.html',
+    styleUrls: ['./carousel.component.scss']
+})
+export class CarouselComponent implements OnInit {
+
+    public slides: Array<Slide> = [];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.slides = Slides;
+    }
+
+}
---
+++ src/app/shared/models/slider-json/slides.ts
@@ -0,0 +1,17 @@
+import { Slide } from '../../interfaces/slide-interface';
+
+export const Slides: Array<Slide> = [
+    {
+        title: 'NIKE AIR FORCE 1 07', paragraph: 'MTAA QS "UNINTERRUPTED',
+        src: 'http://cdn.shopify.com/s/files/1/0208/5268/files/nike-AF1-uninterrupted-banner_10-9.jpg?v=1570726775'
+    },
+    {
+        title: 'ADIDAS ULTRA BOOST 1.0 "OG', paragraph: 'MTAA QS "UNINTERRUPTED',
+        src: 'https://cdn.shopify.com/s/files/1/0208/5268/files/OGULTRABOOST-BANNER2.jpeg?v=1570114766'
+    },
+    {
+        title: 'NEW BALANCE MADE IN USA', paragraph: 'SUPER FABRIC PACK',
+        src: 'https://cdn.shopify.com/s/files/1/0208/5268/files/NBSUPERFABRICPACK-BANNER2.jpeg?v=1570665427'
+    },
+];
+
