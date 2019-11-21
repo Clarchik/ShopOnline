7881776b90478bccdf468d574af5b34ab8b9870c
RudolfKlikh <rudolfklikh@gmail.com>
2019-11-21 15:41:32 +0100

    SHOES-16 , Animation between Login and Reg

--- src/app/shared/models/animations/animation.ts
+++ src/app/shared/models/animations/animation.ts
@@ -1,4 +1,5 @@
-import { query, style, group, animate, transition, trigger } from '@angular/animations';
+import { query, style, group, animate, transition, trigger, useAnimation } from '@angular/animations';
+import { flipInY, fadeInDown, zoomInLeft, zoomIn, rollIn, flipInX, zoomInDown, rotateInUpRight, slideInDown, flip, slideInLeft, lightSpeedIn, bounce, bounceInDown, slideInUp } from 'ng-animate';
 
 export const slideInAnimation =
     trigger('routeAnimation', [
@@ -25,5 +26,17 @@ export const slideInAnimation =
             query(':enter, :leave', [
                 style({ position: 'absolute', top: 0, left: 0, right: 0 })
             ])
+        ]),
+        transition('swipeRight <=> FlipY', [
+            useAnimation(flipInY)
+        ]),
+        transition('swipeLeft <=> FlipY', [
+            query(':enter', style({ transform: 'translateX(-100%)' })),
+            group([
+                query(':leave', [
+                    animate('0.5s cubic-bezier(.35, 0, .25,1)', style({ transform: 'translateX(100%)' })),
+                ]),
+                query(':enter', animate('0.5s cubic-bezier(.35, 0, .25,1)', style({ transform: 'translateX(0%)' }))),
+            ]),
         ])
     ]);
