e6e6f075b04e6e4d6f53c1376efd12c31c19c3fd
dtnfkmdtnfkm3@mail.ru <Clarchik>
2020-01-21 22:44:36 +0100

    gg

--- src/server/modules/static/controller/static.controller.ts
+++ src/server/modules/static/controller/static.controller.ts
@@ -8,16 +8,10 @@ export default class StaticController {
 
 
     private staticRoutes() {
-        // const rootPath = path.join(__dirname, '../../../..');
-        // this.app.use(express.static(`${rootPath}dist/`));
-
-        // this.app.all('*', (req, res) => {
-        //     res.sendFile(`${rootPath}/index.html`);
-        // });
         const { pathdir } = (global as any);
         this.app.use(express.static(pathdir));
 
-        this.app.all('/*', (req, res) => {
+        this.app.all('/', (req, res) => {
             res.sendFile(`${pathdir}/index.html`);
         });
     }
