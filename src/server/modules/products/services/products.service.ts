645e294e7859b23ae807a3daaba40c99325c3fa5
Vitalii Burakovskyy <vitalii.burakovskyy@stazysta.comarch.pl>
2020-01-22 16:17:40 +0100

    gg

--- src/server/modules/products/services/products.service.ts
+++ src/server/modules/products/services/products.service.ts
@@ -1,4 +1,4 @@
-import express, { Response } from 'express';
+import express, {Response} from 'express';
 import Product from '../models/product';
 import CONFIG from '../../../shared/config';
 const paginate = require('jw-paginate');
@@ -6,37 +6,38 @@ const paginate = require('jw-paginate');
 export default class ProductsService {
 
     public getProductsByCategory(req: express.Request, res: Response) {
-            const { category } = req.query;
-            const { title } = req.query;
-            const { page } = req.query;
-            const pageNumber = parseInt(page, undefined) || 1;
-            const param = category === 'all' ? {title} : { title, category };
-            const query = {
-                skip: CONFIG.itemsPerPage * (pageNumber - 1),
-                limit: CONFIG.itemsPerPage
-            };
+        const {category} = req.query;
+        const {title} = req.query;
+        const {page} = req.query;
+        const pageNumber = parseInt(page, undefined) || 1;
+        const titleRegex = {title: {$regex: title, $options: title}};
+        const param = category === 'all' ? {...titleRegex} : {...titleRegex, category};
+        const query = {
+            skip: CONFIG.itemsPerPage * (pageNumber - 1),
+            limit: CONFIG.itemsPerPage
+        };
 
-            Product.countDocuments(param, (error: any, totalCount: number) => {
-                if (error) {
-                    res.status(400).send({ message: 'Error occured' });
+        Product.countDocuments(param, (error: any, totalCount: number) => {
+            if (error) {
+                res.status(400).send({message: 'Error occured'});
+            }
+
+            Product.find(param, null, query, (err: any, products: any) => {
+                if (err) {
+                    res.status(400).send({
+                        message: 'Error occured'
+                    });
                 }
-
-                Product.find(param, null, query, (err: any, products: any) => {
-                    if (err) {
-                        res.status(400).send({
-                            message: 'Error occured'
-                        });
-                    }
-                    const pageSize = CONFIG.itemsPerPage;
-                    const pager = paginate(totalCount, pageNumber, pageSize, CONFIG.pageSizeToShow);
-                    res.status(200).json({ pager, items: products });
-                });
+                const pageSize = CONFIG.itemsPerPage;
+                const pager = paginate(totalCount, pageNumber, pageSize, CONFIG.pageSizeToShow);
+                res.status(200).json({pager, items: products});
             });
+        });
     }
 
 
     public getProductById(req: express.Request, res: Response) {
-        const { id } = req.params;
+        const {id} = req.params;
         Product.findById(id, null, (err: any, data: any) => {
             if (err) {
                 res.status(404).send({
