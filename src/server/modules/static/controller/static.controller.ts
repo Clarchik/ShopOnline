dad6e249f8796b5d29a37f0dbe77b8b620353bc0
Vitalii Burakovskyy <vitalii.burakovskyy@stazysta.comarch.pl>
2020-01-22 11:28:57 +0100

    tt

--- src/server/modules/static/controller/static.controller.ts
+++ src/server/modules/static/controller/static.controller.ts
@@ -9,11 +9,20 @@ export default class StaticController {
 
     private staticRoutes() {
         const {pathdir} = (global as any);
+
+        console.log(pathdir, 'PATH DIR');
+
         this.app.use(express.static(pathdir));
 
-        this.app.get('/:anyreq',  (req, res) => {
-            express.static(pathdir);
+        this.app.get('*', (req, res) => {
+            res.status(200)
+                .set({'content-type': 'text/html; charset=utf-8'})
+                .sendFile(`${pathdir}/index.html`);
         });
+
+        // this.app.get('/:anyreq',  (req, res) => {
+        //     express.static(pathdir);
+        // });
 
         // this.app.all('/', (req, res) => {
         //     res.status(200)
