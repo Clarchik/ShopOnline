2b71fdcb351e02f4aec6436d1c0855cad10690b3
dtnfkmdtnfkm3@mail.ru <Clarchik>
2020-01-24 19:52:13 +0100

    gg

--- src/server/shared/utils.ts
+++ src/server/shared/utils.ts
@@ -1,6 +1,7 @@
 import ejs from 'ejs';
 
 const createHTMLTemplate = (fio: string, products: any, pathdir: string) => {
+    console.log(pathdir, 'path');
     const html = ejs.renderFile(`${pathdir}/server/modules/orders/ejs/order.ejs`, { fio, products });
     return html;
 };
