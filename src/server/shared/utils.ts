6b9b3412c8b9de79e04be9e1f64582f2f03f1491
dtnfkmdtnfkm3@mail.ru <Clarchik>
2020-01-24 20:05:47 +0100

    gg

--- src/server/shared/utils.ts
+++ src/server/shared/utils.ts
@@ -2,7 +2,7 @@ import ejs from 'ejs';
 import path from 'path';
 
 const createHTMLTemplate = (fio: string, products: any, pathdir: string) => {
-    const newPath = path.resolve(__dirname, '..');
+    const newPath = path.resolve(pathdir, '..', '..');
     console.log(newPath, 'new path');
     const html = ejs.renderFile(`${newPath}/server/modules/orders/ejs/order.ejs`, { fio, products });
     return html;
